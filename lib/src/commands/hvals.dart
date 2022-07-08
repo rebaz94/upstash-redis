c78b273c80f15d6818b792e108d952150457cc28
Rebaz Raouf <rebazrauf@gmail.com>
2022-07-08 22:47:53 +0300

    Remove custom parsing

--- lib/src/commands/hvals.dart
+++ lib/src/commands/hvals.dart
@@ -6,16 +6,4 @@ class HValsCommand<TData> extends Command<List<TData>, List<TData>> {
   factory HValsCommand(String key, [CommandOption<List<TData>, List<TData>>? opts]) {
     return HValsCommand._(['hvals', key], opts);
   }
-
-  @override
-  Future<List<TData>> exec(Requester client) async {
-    final response = await client.request<dynamic>(body: command);
-    var result = checkUpstashResponse<dynamic>(response);
-
-    if (result is List) {
-      result = List<String>.from(result);
-    }
-
-    return deserialize(result);
-  }
 }
