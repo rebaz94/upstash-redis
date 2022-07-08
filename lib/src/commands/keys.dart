e6eacbde334a9710832b5eb37aa2ad7cad2593a7
Rebaz Raouf <rebazrauf@gmail.com>
2022-07-08 22:50:42 +0300

    Remove custom parsing

--- lib/src/commands/keys.dart
+++ lib/src/commands/keys.dart
@@ -6,11 +6,4 @@ class KeysCommand extends Command<List<String>, List<String>> {
   factory KeysCommand(String pattern, [CommandOption<List<String>, List<String>>? opts]) {
     return KeysCommand._(['keys', pattern], opts);
   }
-
-  @override
-  Future<List<String>> exec(Requester client) async {
-    final response = await client.request<List>(body: command);
-    final result = checkUpstashResponse<List>(response);
-    return deserialize(List<String>.from((result ?? [])));
-  }
 }
