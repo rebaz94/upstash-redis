e1383a827867dcbab6a7293b2ca4c1c9075a34a0
rebaz94 <rebazrauf@gmail.com>
2023-02-23 00:05:22 +0300

    fix: typo

--- test/commands/zdiffstore_test.dart
+++ test/commands/zdiffstore_test.dart
@@ -11,28 +11,26 @@ void main() async {
 
   tearDownAll(() => keygen.cleanup());
 
-  group('stores the diff', () {
-    test('returns the cardinality', () async {
-      final key1 = newKey();
-      final key2 = newKey();
-      final out = newKey();
-      await ZAddCommand(key1, [
-        ScoreMember(score: 1, member: 'one'),
-        ScoreMember(score: 2, member: 'two'),
-        ScoreMember(score: 3, member: 'three'),
-      ]).exec(client);
-      await ZAddCommand(key2, [
-        ScoreMember(score: 1, member: 'one'),
-        ScoreMember(score: 2, member: 'two'),
-      ]).exec(client);
+  test('stores the diff', () async {
+    final key1 = newKey();
+    final key2 = newKey();
+    final out = newKey();
+    await ZAddCommand(key1, [
+      ScoreMember(score: 1, member: 'one'),
+      ScoreMember(score: 2, member: 'two'),
+      ScoreMember(score: 3, member: 'three'),
+    ]).exec(client);
+    await ZAddCommand(key2, [
+      ScoreMember(score: 1, member: 'one'),
+      ScoreMember(score: 2, member: 'two'),
+    ]).exec(client);
 
-      final res = await ZDiffStoreCommand(out, 2, [key1, key2]).exec(client);
-      expect(res, 1);
+    final res = await ZDiffStoreCommand(out, 2, [key1, key2]).exec(client);
+    expect(res, 1);
 
-      final zset3 = await ZRangeCommand<String>(out, 0, -1, withScores: true)
-          .exec(client);
-      expect(zset3[0], 'three');
-      expect(zset3[1], '3');
-    });
+    final zset3 =
+        await ZRangeCommand<String>(out, 0, -1, withScores: true).exec(client);
+    expect(zset3[0], 'three');
+    expect(zset3[1], '3');
   });
 }
